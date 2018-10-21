55c471ad99ed301b69940f39653e81f9b2941d6a
Anirudh Bhat <anirudh.b.123@gmail.com>
2018-10-21 21:29:08 +0530

    marks calculated successfully

--- src/student/instructionPageController.java
+++ src/student/instructionPageController.java
@@ -23,6 +23,7 @@ import connectivity.Connect;
 public class instructionPageController implements Initializable {
 
     String subName;
+
     int totalQuestions = 0;
 
     @FXML
@@ -40,11 +41,10 @@ public class instructionPageController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Platform.runLater(() -> {
+        Platform.runLater(()->{
             try {
-                getQuestions();
+                setText();
             } catch (SQLException e) {}
-            setQuestions();
         });
     }
 
@@ -52,57 +52,28 @@ public class instructionPageController implements Initializable {
         this.subName = subName;
     }
 
-    public Vector<Question> questions = new Vector<Question>();
-
-    void getQuestions() throws SQLException {
+    void setText() throws SQLException {
         Connect connect = new Connect();
         Connection connection = connect.getConnection();
         String query = "SELECT * FROM `question` WHERE (subject = '"+ subName +"')";
         Statement statement = connection.createStatement();
         ResultSet rs = statement.executeQuery(query);
-        Question q1 = new Question();
         while(rs.next()) {
-            q1.getQuestion(rs.getString("data"));
-            q1.getAns(rs.getInt("ans"));
-            q1.getOptions(rs.getString("option1"), rs.getString("option2"), rs.getString("option3"), rs.getString("option4"));
-            questions.addElement(q1);
             this.totalQuestions++;
         }
-        System.out.println(subName);
-        System.out.println("total questions "+totalQuestions);
-        totalDisplay.setText("Number of Questions: " + totalQuestions);
         statement.close();
         connection.close();
+        totalDisplay.setText("Number of Questions: " + totalQuestions);
     }
 
-    void setQuestions() {
-        ScrollPane sp = new ScrollPane();
-        VBox vbox = control.questionVbox;
-        Label questionLabel[] = new Label[totalQuestions];
-        RadioButton questionBtn[] = new RadioButton[4*totalQuestions];
-        ToggleGroup tg[] = new ToggleGroup[totalQuestions];
-        for(int i=0; i<totalQuestions; i++) {
-            questionLabel[i] = new Label(questions.elementAt(i).question);
-            questionBtn[(i*4)+0] = new RadioButton(questions.elementAt(i).option1);
-            questionBtn[(i*4)+1] = new RadioButton(questions.elementAt(i).option2);
-            questionBtn[(i*4)+2] = new RadioButton(questions.elementAt(i).option3);
-            questionBtn[(i*4)+3] = new RadioButton(questions.elementAt(i).option4);
-            tg[i] = new ToggleGroup();
-            questionBtn[(i*4)+0].setToggleGroup(tg[i]);
-            questionBtn[(i*4)+1].setToggleGroup(tg[i]);
-            questionBtn[(i*4)+2].setToggleGroup(tg[i]);
-            questionBtn[(i*4)+3].setToggleGroup(tg[i]);
-            vbox.getChildren().addAll(questionLabel[i], questionBtn[(i*4)+0], questionBtn[(i*4)+1], questionBtn[(i*4)+2], questionBtn[(i*4)+3]);
-        }
-        sp.setContent(vbox);
-        sp.setPrefSize(100,100);
-        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
-    }
+
 
     @FXML
     private void startTest() {
         testPageController control = fxmlLoader.<testPageController>getController();
+        control.getSubName(subName);
         control.startTimer();
+        control.start();
         rootPane.getChildren().setAll(testPane);
     }
 
@@ -110,24 +81,21 @@ public class instructionPageController implements Initializable {
 
 class Question {
     String question;
-    int ans;
-    String option1;
-    String option2;
-    String option3;
-    String option4;
+    String ans;
+    String[] option = new String[4];
 
     void getQuestion(String question) {
         this.question = question;
     }
 
-    void getAns(int ans) {
+    void getAns(String ans) {
         this.ans = ans;
     }
 
     void getOptions(String option1, String option2, String option3, String option4){
-        this.option1 = option1;
-        this.option2 = option2;
-        this.option3 = option3;
-        this.option4 = option4;
+        this.option[0] = option1;
+        this.option[1] = option2;
+        this.option[2] = option3;
+        this.option[3] = option4;
     }
 }
--- src/student/testPageController.java
+++ src/student/testPageController.java
@@ -1,23 +1,28 @@
 package student;
 
+import java.awt.desktop.SystemSleepEvent;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+
+import connectivity.Connect;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-
-import javax.swing.text.html.FormSubmitEvent;
 
 public class testPageController implements Initializable {
 
@@ -32,9 +37,19 @@ public class testPageController implements Initializable {
     @FXML
     Label timeLabel;
 
+    int totalQuestions = 0;
+
+    String subName;
+
+    int totalMarks=0;
+
     int interval = 1200;
 
     Timer timer = new Timer();
+
+    int percentage;
+
+    public Vector<Question> questions = new Vector<Question>();
 
     public void startTimer() {
         this.timer.scheduleAtFixedRate(new TimerTask() {
@@ -48,7 +63,7 @@ public class testPageController implements Initializable {
                 else{
                     try {
                         submit();
-                    } catch (IOException e) {}
+                    } catch (IOException | SQLException e) {}
                     timer.cancel();
                 }
             }
@@ -62,25 +77,110 @@ public class testPageController implements Initializable {
     }
 
     @FXML
-    private void handleSubmit() throws IOException {
+    private void handleSubmit() throws IOException, SQLException {
         boolean result = ConfirmBox.display("Submit", "Are you sure you want to submit?");
         if(result == true){
             submit();
         }
     }
 
-    void submit() throws IOException {
+    void submit() throws IOException, SQLException {
         this.timer.cancel();
         evaluate();
+        saveMarks();
         changeScene();
     }
 
-    void evaluate(){
+    void saveMarks(){
+//        Connect connect = new Connect();
+//        Connection connection = connect.getConnection();
+    }
+
+    void getSubName(String subName){
+        this.subName = subName;
+        System.out.println(this.subName);
+        String query = "SELECT * FROM `question` WHERE (subject = '"+ subName +"')";
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        Platform.runLater(()->{
+            start();
+        });
     }
 
+    void start(){
+        System.out.println(subName);
+        try {
+            getQuestions();
+        } catch (SQLException e) {}
+        setQuestions();
+    }
+
+    void getQuestions() throws SQLException {
+        Connect connect = new Connect();
+        Connection connection = connect.getConnection();
+        System.out.println("SUBNAME"+subName);
+        String query = "SELECT * FROM `question` WHERE (subject = '"+ subName +"')";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        Question q1[] = new Question[20];
+        int i=0;
+        while(rs.next()) {
+            q1[i] = new Question();
+            q1[i].getQuestion(rs.getString("data"));
+            q1[i].getAns(rs.getString("ans"));
+            q1[i].getOptions(rs.getString("option1"), rs.getString("option2"), rs.getString("option3"), rs.getString("option4"));
+            questions.addElement(q1[i]);
+            this.totalQuestions++;
+            i++;
+        }
+        System.out.println(subName);
+        System.out.println("total questions "+totalQuestions);
+        statement.close();
+        connection.close();
+    }
+
+    void setQuestions() {
+        ScrollPane sp = new ScrollPane();
+        Label questionLabel[] = new Label[totalQuestions];
+        RadioButton questionBtn[] = new RadioButton[4*totalQuestions];
+        ToggleGroup tg[] = new ToggleGroup[totalQuestions];
+        for(int i=0; i<totalQuestions; i++) {
+            System.out.println(questions.elementAt(i).ans);
+            questionLabel[i] = new Label((i+1)+ ". " +questions.elementAt(i).question);
+            questionBtn[(i*4)+0] = new RadioButton(questions.elementAt(i).option[0]);
+            questionBtn[(i*4)+1] = new RadioButton(questions.elementAt(i).option[1]);
+            questionBtn[(i*4)+2] = new RadioButton(questions.elementAt(i).option[2]);
+            questionBtn[(i*4)+3] = new RadioButton(questions.elementAt(i).option[3]);
+            tg[i] = new ToggleGroup();
+            questionBtn[(i*4)+0].setToggleGroup(tg[i]);
+            questionBtn[(i*4)+1].setToggleGroup(tg[i]);
+            questionBtn[(i*4)+2].setToggleGroup(tg[i]);
+            questionBtn[(i*4)+3].setToggleGroup(tg[i]);
+            int finalI = i;
+            tg[i].selectedToggleProperty().addListener(new ChangeListener<Toggle>()
+            {
+                @Override
+                public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1)
+                {
+
+                    RadioButton chk = (RadioButton)t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+                    System.out.println(chk.getText().equals("option"+questions.elementAt(finalI).ans));
+                    if(chk.getText().equals("option"+questions.elementAt(finalI).ans)){
+                        totalMarks++;
+                    }
+                }
+            });
+            questionVbox.getChildren().addAll(questionLabel[i], questionBtn[(i*4)+0], questionBtn[(i*4)+1], questionBtn[(i*4)+2], questionBtn[(i*4)+3]);
+        }
+        sp.setContent(questionVbox);
+        sp.setPrefSize(100,100);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.ALWAYS);
+    }
+
+    void evaluate() {
+        percentage = totalMarks/totalQuestions;
+    }
 }
